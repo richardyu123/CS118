3011dec5c12e0b224609e30a4c3223c56038a498
Kaitlyne Chan <kchan52@ucla.edu>
2018-01-31 22:55:45 -0800

    Fixed TA's compilation error

--- Project_1/client.cc
+++ Project_1/client.cc
@@ -51,9 +51,9 @@ int main(int argc, char *argv[])
 
     printf("Please enter the message: ");
     memset(buffer,0, 256);
-    n = fgets(buffer,255,stdin);  // read message
-    if (n < 0)
-        error("ERROR reading in message");
+    fgets(buffer,255,stdin);  // read message
+    //if (n < 0)
+    //    error("ERROR reading in message");
 
     n = write(sockfd,buffer,strlen(buffer));  // write to the socket
     if (n < 0)
